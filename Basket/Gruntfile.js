5a44d947f629d4e755f0ae2f35d832b03dc7aa41
sharmajitesh07 <sharmajitesh07@gmail.com>
2017-06-13 07:26:27 +0530

    updated few minor points

--- Basket/Gruntfile.js
+++ Basket/Gruntfile.js
@@ -7,8 +7,8 @@ module.exports = function(grunt) {
     jshint: {
       all: [
         'Gruntfile.js',
-        'js/basket.js'
-        /*'js/basket_item.js'*/
+        'js/basket.js',
+        'js/basket_item.js'
       ],
       options: {
         jshintrc: '.jshintrc',
